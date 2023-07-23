72f5630a54cfad8853ea58851b302867b387b55d
liuxsdev <liuxspro@gmail.com>
2023-07-23 14:30:58 +0800

    update python

--- home-manager/programs/python.nix
+++ home-manager/programs/python.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
 
 # Python
+# https://nixos.wiki/wiki/Python
+# {
+#   home.packages = with pkgs;[
+#     python311
+#     python311Packages.pip
+#     python311Packages.requests
+#   ];
+# }
 {
-  home.packages = with pkgs;[
-    python311
-    python311Packages.pip
-    python311Packages.requests
+  let
+  my-python-packages = ps: with pkgs.ps; [
+    pip
+    requests
   ];
+  python-with-my-packages = pkgs.python311.withPackages my-python-packages;
+  in
+  {
+  home.packages = with pkgs; [
+    python-with-my-packages
+  ];
+}
 }
