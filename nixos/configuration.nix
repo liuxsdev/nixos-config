47174a3fc8a1ca42fbfb2597020cb11302408c85
liuxsdev <liuxspro@gmail.com>
2023-07-23 18:09:55 +0800

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -44,10 +44,22 @@
   };
 
   i18n.inputMethod = {
-    enabled = "ibus";
-    ibus.engines = with pkgs.ibus-engines;[
-      libpinyin
-      rime
+    # enabled = "ibus";
+    # ibus.engines = with pkgs.ibus-engines;[
+    #   libpinyin
+    #   rime
+    # ];
+    # 使用 fcitx5 输入法
+    # https://github.com/ryan4yin/nix-config/blob/v0.0.5/home/linux/fcitx5/default.nix
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs;[
+      # for flypy chinese input method
+      fcitx5-rime
+      # needed enable rime using configtool after installed
+      fcitx5-configtool
+      fcitx5-chinese-addons
+      # fcitx5-mozc    # japanese input method
+      fcitx5-gtk # gtk im module
     ];
   };
   # Font
