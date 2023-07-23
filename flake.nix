8c909b7b062c122b819b74884b0095628e6e3a37
liuxsdev <liuxspro@gmail.com>
2023-07-23 13:32:24 +0800

    add mirror

--- flake.nix
+++ flake.nix
@@ -1,6 +1,28 @@
 {
   description = "Nix config";
 
+  # https://nixos-and-flakes.thiscute.world/zh/nixos-with-flakes/nixos-with-flakes-enabled#add-cache-source-for-flake
+  # 
+  # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+
+    substituters = [
+      # replace official cache with a mirror located in China
+      "https://mirrors.bfsu.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
+
+    # nix community's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
